70edeeaf6e25e44c61180aa5e7a90b3f36a43d58
Blake West <bwest32@gatech.edu>
2023-07-03 19:00:23 -0400

    Added pickling and unpickling functions

--- img_utls.py
+++ img_utls.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import pandas as pd
 import numpy as np
+import pickle
 from typing import List, Iterable, Tuple
 
 SCRIPT_DIR       = os.path.dirname(os.path.abspath(__file__))
@@ -11,14 +12,15 @@ IMAGE_DIR        = os.path.join(SCRIPT_DIR, 'images')
 TRAIN_IMAGES_DIR = os.path.join(IMAGE_DIR, 'training')
 VALID_IMAGES_DIR = os.path.join(IMAGE_DIR, 'validation')
 EXTRACTED_BB_DIR = os.path.join(IMAGE_DIR, 'extracted')
+DATA_DIR         = os.path.join(SCRIPT_DIR, 'data')
 
 BB_TRAIN_IMAGES_DIR = os.path.join(EXTRACTED_BB_DIR, 'training')
 BB_VALID_IMAGES_DIR = os.path.join(EXTRACTED_BB_DIR, 'validation')
 
 
 
-def ingest_annotations(training   : str, 
-                       validation : str) -> Iterable[pd.DataFrame]:
+def ingest_annotations(training   : str = TRAIN_ANNOTE_PTH, 
+                       validation : str = VALID_ANNOTE_PTH) -> Iterable[pd.DataFrame]:
   """ Ingest annotations and return the two dataframes
 
   Args:
@@ -32,8 +34,8 @@ def ingest_annotations(training   : str,
   columns = ['category_id', 'bbox', 'area', 'iscrowd']
   return [pd.read_json(x, orient='records')[columns] for x in (training, validation)]
 
-def extract_bb_images(training_df : pd.DataFrame, validation_df : pd.DataFrame,
-                      save_bb_images : bool = False, add_raw_img_to_df : bool = False) -> None:
+def extract_bb_images(training_df : pd.DataFrame = None, validation_df : pd.DataFrame = None,
+                      save_bb_images : bool = False, add_raw_img_to_df : bool = False) -> Tuple[pd.DataFrame]:
   """ Extract the images from the bounding boxes defined in the annotations
 
   Args:
@@ -46,6 +48,12 @@ def extract_bb_images(training_df : pd.DataFrame, validation_df : pd.DataFrame,
     
   """
   
+  if training_df is None or validation_df is None:
+    _train, _valid = ingest_annotations()
+    if not training_df:
+      training_df = _train
+    if not validation_df:
+      validation_df = _valid
 
   dirs = [TRAIN_IMAGES_DIR, VALID_IMAGES_DIR]
   extract_out = [BB_TRAIN_IMAGES_DIR, BB_VALID_IMAGES_DIR]
@@ -80,6 +88,8 @@ def extract_bb_images(training_df : pd.DataFrame, validation_df : pd.DataFrame,
     if add_raw_img_to_df:
       df['raw_image'] = images
     df['bb_image']  = bb_images
+  
+  return (training_df, validation_df)
   # End Outer Loop
   
   
@@ -116,6 +126,50 @@ def extract_bb_image(image : np.ndarray, bb : List[int]) -> np.ndarray:
                           top_left[0]:bottom_right[0]].copy()
 
   return extracted_image
+
+def create_img_data_pickle(keep_raw_images : bool = False) -> str:
+  """ Create an image data pickle file.
+
+  Args:
+      keep_raw_images (bool, optional): Flag to keep the raw image data in the dataframes. 
+                                        Defaults to False.
+
+  Returns:
+      str: filepath to the pickle file saved
+  """
+  if keep_raw_images:
+    training_df, validation_df = extract_bb_images(keep_raw_images=keep_raw_images)
+  else:
+    training_df, validation_df = extract_bb_images()
+  
+  if not os.path.isdir(DATA_DIR):
+    os.makedirs(DATA_DIR, exist_ok=True)
+  
+  output_path = os.path.join(DATA_DIR, 'img_data.pkl')
+  
+  with open(output_path, 'wb') as f:
+    _dict = {'training_df' : training_df, 'validation_df' : validation_df}
+    pickle.dump(_dict, f)
+  
+  return output_path
+
+def load_img_data_pkl(path : str) -> Tuple[pd.DataFrame]:
+  """ Load the given pickle and return the training and validation
+      dataframe in a tuple
+
+  Args:
+      path (str): filepath to img_data.pkl
+
+  Returns:
+      Tuple[pd.DataFrame]: (training_df, validation_df)
+  """
+  
+  with open(path, 'rb') as f:
+    _dict = pickle.load(f)
+
+  return tuple(_dict.items())
+  
+  
 def main() -> int:
   '''
     Entry point for the program.
@@ -127,7 +181,6 @@ def main() -> int:
   
   
   return 0
-
 
 if __name__ == '__main__':
   ret = main()
