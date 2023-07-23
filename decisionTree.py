4c6d6b83073fd526ff28e2829a814c2649a3ffb6
Blake West <bwest32@gatech.edu>
2023-07-23 13:15:40 -0400

    Decision tree updated to work with training and validation data sets

--- decisionTree.py
+++ decisionTree.py
@@ -27,76 +27,119 @@ else:
     training_df, validation_df = load_data()
 for df in [training_df, validation_df]:
     img_utls.preprocess_images(df)
-FE = fe.Feature_Extractor(training_df)
+FE = fe.Feature_Extractor(training_df, validation_df)
 features = FE.load_features()
 
-y = training_df['category_id'].to_numpy()
+y_train = training_df['category_id'].to_numpy()
+y_test = validation_df['category_id'].to_numpy()
 
 def convert_features(features) -> Tuple[np.ndarray, np.ndarray]:
     # Existing code to load the features dictionary
     feature_dict = features
     
     # Extract the features from the dictionary
-    bb_features = feature_dict['bb_Mobilenet']
-    mobilenet_features = feature_dict['Mobilenet']
-    hog_features = feature_dict['HOG']
-    orb_features = feature_dict['ORB']
-    edges_features = feature_dict['edges']
-    contours = feature_dict['contours']
+    t_bb_features = feature_dict['t_bb_Mobilenet']
+    t_mobilenet_features = feature_dict['t_Mobilenet']
+    t_hog_features = feature_dict['t_HOG']
+    t_orb_features = feature_dict['t_ORB']
+    t_edges_features = feature_dict['t_edges']
+    t_contours = feature_dict['t_contours']
+    
+    v_bb_features = feature_dict['v_bb_Mobilenet']
+    v_mobilenet_features = feature_dict['v_Mobilenet']
+    v_hog_features = feature_dict['v_HOG']
+    v_orb_features = feature_dict['v_ORB']
+    v_edges_features = feature_dict['v_edges']
+    v_contours = feature_dict['v_contours']
+    
 
     # Run for bb_features and mobilenet_features
-    X_train_bb = pd.DataFrame(bb_features.tolist()).values
-    X_train_mobile = pd.DataFrame(mobilenet_features.tolist()).values
+    X_train_bb      = np.array(t_bb_features.tolist())
+    X_train_mobile  = np.array(t_mobilenet_features.tolist())
+    y_test_bb       = np.array(v_bb_features.tolist())
+    y_test_mobile   = np.array(v_mobilenet_features.tolist())
 
     # Run for hog_features, orb_features, edges_features
-    temp1 = np.array(hog_features.to_list())
+    temp1       = np.array(t_hog_features.to_list())
     X_train_hog = temp1.reshape(temp1.shape[0], -1)
+    temp2       = np.array(v_hog_features.to_list())
+    y_test_hog  = temp2.reshape(temp2.shape[0], -1)
 
-    temp2 = np.array(orb_features.to_list())
-    X_train_orb = temp2.reshape(temp2.shape[0], -1)
+    temp3       = np.array(t_orb_features.to_list())
+    X_train_orb = temp3.reshape(temp3.shape[0], -1)
+    temp4       = np.array(v_orb_features.to_list())
+    y_test_orb  = temp4.reshape(temp4.shape[0], -1)
 
-    temp3 = np.array(edges_features.to_list())
-    X_train_edges = temp3.reshape(temp3.shape[0], -1)
+    temp5 = np.array(t_edges_features.to_list())
+    X_train_edges = temp5.reshape(temp5.shape[0], -1)
+    temp6 = np.array(v_edges_features.to_list())
+    y_test_edges = temp6.reshape(temp6.shape[0], -1)
 
     # Run for contour_features
-    contours_features = contours.apply(lambda x: x[0] if len(x) > 0 else [])
+    t_contours_features = t_contours.apply(lambda x: x[0] if len(x) > 0 else [])
+    v_contours_features = v_contours.apply(lambda x: x[0] if len(x) > 0 else [])
 
     # Extract specific contour features
-    contours_area = contours_features.apply(lambda c: cv2.contourArea(c) if len(c) > 0 else 0)
-    contours_perimeter = contours_features.apply(lambda c: cv2.arcLength(c, closed=True) if len(c) > 0 else 0)
+    t_contours_area = t_contours_features.apply(lambda c: cv2.contourArea(c) if len(c) > 0 else 0)
+    t_contours_perimeter = t_contours_features.apply(lambda c: cv2.arcLength(c, closed=True) if len(c) > 0 else 0)
+    
+    v_contours_area = v_contours_features.apply(lambda c: cv2.contourArea(c) if len(c) > 0 else 0)
+    v_contours_perimeter = v_contours_features.apply(lambda c: cv2.arcLength(c, closed=True) if len(c) > 0 else 0)
 
     # Filter out contours with area or perimeter as zero
-    valid_contours = contours_features[(contours_area > 0) & (contours_perimeter > 0)]
-    valid_area = contours_area[(contours_area > 0) & (contours_perimeter > 0)]
-    valid_perimeter = contours_perimeter[(contours_area > 0) & (contours_perimeter > 0)]
+    t_valid_contours = t_contours_features[(t_contours_area > 0) & (t_contours_perimeter > 0)]
+    t_valid_area = t_contours_area[(t_contours_area > 0) & (t_contours_perimeter > 0)]
+    t_valid_perimeter = t_contours_perimeter[(t_contours_area > 0) & (t_contours_perimeter > 0)]
+    
+    v_valid_contours = v_contours_features[(v_contours_area > 0) & (v_contours_perimeter > 0)]
+    v_valid_area = v_contours_area[(v_contours_area > 0) & (v_contours_perimeter > 0)]
+    v_valid_perimeter = v_contours_perimeter[(v_contours_area > 0) & (v_contours_perimeter > 0)]
 
     # Calculate Hu Moments and Compactness for valid contours
-    hu_moments = valid_contours.apply(lambda c: cv2.HuMoments(cv2.moments(c)).flatten())
-    compactness = valid_perimeter**2 / valid_area
+    t_hu_moments = t_valid_contours.apply(lambda c: cv2.HuMoments(cv2.moments(c)).flatten())
+    t_compactness = t_valid_perimeter**2 / t_valid_area
 
+    v_hu_moments = v_valid_contours.apply(lambda c: cv2.HuMoments(cv2.moments(c)).flatten())
+    v_compactness = v_valid_perimeter**2 / v_valid_area
     # Create a DataFrame to hold the features
-    features_df = pd.DataFrame({
-        'contour_area': contours_area,
-        'contour_perimeter': contours_perimeter,
-        'hu_moment1': hu_moments.apply(lambda x: x[0]),
-        'hu_moment2': hu_moments.apply(lambda x: x[1]),
-        'hu_moment3': hu_moments.apply(lambda x: x[2]),
-        'hu_moment4': hu_moments.apply(lambda x: x[3]),
-        'hu_moment5': hu_moments.apply(lambda x: x[4]),
-        'hu_moment6': hu_moments.apply(lambda x: x[5]),
-        'hu_moment7': hu_moments.apply(lambda x: x[6]),
-        'compactness': compactness,
+    t_features_df = pd.DataFrame({
+        'contour_area': t_contours_area,
+        'contour_perimeter': t_contours_perimeter,
+        'hu_moment1': t_hu_moments.apply(lambda x: x[0]),
+        'hu_moment2': t_hu_moments.apply(lambda x: x[1]),
+        'hu_moment3': t_hu_moments.apply(lambda x: x[2]),
+        'hu_moment4': t_hu_moments.apply(lambda x: x[3]),
+        'hu_moment5': t_hu_moments.apply(lambda x: x[4]),
+        'hu_moment6': t_hu_moments.apply(lambda x: x[5]),
+        'hu_moment7': t_hu_moments.apply(lambda x: x[6]),
+        'compactness': t_compactness,
+    })
+    
+    v_features_df = pd.DataFrame({
+        'contour_area': v_contours_area,
+        'contour_perimeter': v_contours_perimeter,
+        'hu_moment1': v_hu_moments.apply(lambda x: x[0]),
+        'hu_moment2': v_hu_moments.apply(lambda x: x[1]),
+        'hu_moment3': v_hu_moments.apply(lambda x: x[2]),
+        'hu_moment4': v_hu_moments.apply(lambda x: x[3]),
+        'hu_moment5': v_hu_moments.apply(lambda x: x[4]),
+        'hu_moment6': v_hu_moments.apply(lambda x: x[5]),
+        'hu_moment7': v_hu_moments.apply(lambda x: x[6]),
+        'compactness': v_compactness,
     })    
     scaler = MinMaxScaler()
-    X_train_contours = scaler.fit_transform(features_df)
+    X_train_contours    = scaler.fit_transform(t_features_df)
+    y_test_contours     = scaler.fit_transform(v_features_df)
     
-    gray = training_df['64x64'].apply(lambda x : cv2.cvtColor(x, cv2.COLOR_BGR2GRAY).flatten()).tolist()
+    t_gray = np.array(training_df['64x64'].apply(lambda x : cv2.cvtColor(x, cv2.COLOR_BGR2GRAY).flatten()).tolist())
+    v_gray = np.array(validation_df['64x64'].apply(lambda x : cv2.cvtColor(x, cv2.COLOR_BGR2GRAY).flatten()).tolist())
 
-    return X_train_bb, X_train_mobile, X_train_hog, X_train_orb, X_train_edges, X_train_contours, gray
+    return X_train_bb, X_train_mobile, X_train_hog, X_train_orb, X_train_edges, X_train_contours, t_gray, \
+        y_test_bb, y_test_mobile, y_test_hog, y_test_orb, y_test_edges, y_test_contours, v_gray
 
 
-def find_accuracy(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+def find_accuracy(X_train, y_train, X_test, y_test):
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Create a decision tree classifier
     clf = tree.DecisionTreeClassifier()
@@ -114,7 +157,7 @@ def find_accuracy(X, y):
 
 
 
-def mobile_net_bb_hp(X, y) -> None:
+def mobile_net_bb_hp(x_train, y_train, x_test, y_test) -> None:
     _args = []
     A = np.arange(3, 16)
     B = np.arange(3, 16)
@@ -149,22 +192,23 @@ def fit_RF(my_tuple : tuple) -> float:
     return [acc, estimators, max_depth, max_features]
 
 
-bb, mobile, hog, orb, edges, contours, gray = convert_features(features)
-X_train, X_test, y_train, y_test = train_test_split(bb, y, test_size=0.2, random_state=42)
+t_bb, t_mobile, t_hog, t_orb, t_edges, t_contours, t_gray, \
+v_bb, v_mobile, v_hog, v_orb, v_edges, v_contours, v_gray, \
+    = convert_features(features)
 
-mobile_net_bb_hp(bb, y)
+#mobile_net_bb_hp(t_bb, y_train, v_bb, y_test)
 
 print("grayscale")
-find_accuracy(gray, y)
+find_accuracy(t_gray, y_train, v_gray, y_test)
 print("bb")
-find_accuracy(bb, y)
+find_accuracy(t_bb, y_train, v_bb, y_test)
 print("mobile")
-find_accuracy(mobile, y)
+find_accuracy(t_mobile, y_train, v_mobile, y_test)
 print("hog")
-find_accuracy(hog, y)
+find_accuracy(t_hog, y_train, v_hog, y_test)
 print("orb")
-find_accuracy(orb, y)
+find_accuracy(t_orb, y_train, v_orb, y_test)
 print("edges")
-find_accuracy(edges, y)
+find_accuracy(t_edges, y_train, v_edges, y_test)
 print("contours")
-find_accuracy(contours, y)
+find_accuracy(t_contours, y_train, v_contours, y_test)
